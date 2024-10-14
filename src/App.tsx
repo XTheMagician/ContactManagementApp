b59936fa6769c6f5dfd166c80c1ff2d14c1af19b
s392480 <leo.schwarzkopf@stud-mail.uni-wuerzburg.de>
2024-10-14 12:45:21 +0200

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4" align="center" gutterBottom>
+        Contact Management App
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
